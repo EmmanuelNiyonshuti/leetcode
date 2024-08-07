e5f7cc96844b694e8563d778c56c0552f56ee785
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2024-08-07 22:41:33 +0200

    optimized is_subsequence.py a bit

--- is_subsequence.py
+++ is_subsequence.py
@@ -2,12 +2,8 @@
 
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        i, j = 0, 0
-        while j < len(s) and i < len(t):
-            if s[j] == t[i]:
-                j += 1
-            i += 1
-        return j == len(s)
+       t_iter = iter(t)
+       return all(char in t_iter for char in s)
 
 
 if __name__ == "__main__":
